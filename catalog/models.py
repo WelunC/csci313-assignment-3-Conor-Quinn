25412325029fe08c51d36d31bb21b6cca10d42b2
WelunC <107059413+WelunC@users.noreply.github.com>
2024-04-02 15:24:56 -0500

    Admin.py changes, challenge questions done as well

--- catalog/models.py
+++ catalog/models.py
@@ -46,7 +46,7 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(
         Genre, help_text="Select a genre for this book")
-
+    
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -54,6 +54,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
 
@@ -99,9 +105,10 @@ class Author(models.Model):
         ordering = ['last_name', 'first_name']
 
     def get_absolute_url(self):
-        """Returns the URL to access a particular author instance."""
+        """Returns the URL to access a particular pauthor instance."""
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
