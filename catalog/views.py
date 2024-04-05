309755622674dc676a45e3e019b9be4bc2434734
WelunC <107059413+WelunC@users.noreply.github.com>
2024-04-05 09:16:06 -0500

    Style Updates and Final Build, still can't get it to load the correct unicode

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 def index(request):
     """View function for home page of site."""
 
@@ -106,29 +107,32 @@ def renew_book_librarian(request, pk):
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Author
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
-#class AuthorCreate(PermissionRequiredMixin, CreateView):
-#    model = Author
-#    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
-#    initial = {'date_of_death': '11/11/2023'}
-#    permission_required = 'catalog.add_author'
 
-#class AuthorUpdate(PermissionRequiredMixin, UpdateView):
-#    model = Author
-#    # Not recommended (potential security issue if more fields added)
-#    fields = '__all__'
-#    permission_required = 'catalog.change_author'
 
-#class AuthorDelete(PermissionRequiredMixin, DeleteView):
-#    model = Author
-#    success_url = reverse_lazy('authors')
-#    permission_required = 'catalog.delete_author'
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/11/2023'}
+    permission_required = 'catalog.add_author'
 
-#    def form_valid(self, form):
-#        try:
-#            self.object.delete()
-#            return HttpResponseRedirect(self.success_url)
-#        except Exception as e:
-#            return HttpResponseRedirect(
-#                reverse("author-delete", kwargs={"pk": self.object.pk})
-#            )
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    # Not recommended (potential security issue if more fields added)
+    fields = '__all__'
+    permission_required = 'catalog.change_author'
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("author-delete", kwargs={"pk": self.object.pk})
+            )
